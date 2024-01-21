4d4358b86910ccde74f00f430dfff329e4e7f982
fredritchie <130365973+fredritchie@users.noreply.github.com>
2024-01-21 18:44:00 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "Hypha-Mentor"
-    region = "us-east-1"
+    bucket = "ritchie-hypha-k8s-project"
+    region = "ap-south-1"
     key    = "eks/terraform.tfstate"
   }
 }
